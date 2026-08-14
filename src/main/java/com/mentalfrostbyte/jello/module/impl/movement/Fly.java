cbc8055d3fbd87148f2ec4a7106ac5b2d6f05116
juzibujiji <1590079027@qq.com>
2026-08-14 18:45:08 +0800

    fuck

--- src/main/java/com/mentalfrostbyte/jello/module/impl/movement/Fly.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/movement/Fly.java
@@ -11,7 +11,7 @@ public class Fly extends ModuleWithModuleSettings {
                 "Fly",
                 "Allows you to fly like a bird",
                 new VanillaFly(),
-                new PolarFly(),
+                //new PolarFly(),
                 new VClipFly(),
                 new HypixelFly(),
                 new MineplexFly(),
